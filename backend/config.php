faa33ecf21a1556f8a4fa60634c9c317b7889b6f
kattogat <maria_hav@live.se>
2017-02-02 13:51:44 +0100

    La till komentarer

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,9 @@
 <?php	
+	//config-filen med PDO. Använd för att kalla på loox databasen!
 
-    $host = 'localhost'; 
+    $host = 'localhost'; //Eller datorns ip adress
     $db = 'loox'; 
-    $user = 'root';
+    $user = 'root'; 
     $password = 'root';
     $charset = 'utf8';
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -10,5 +11,7 @@
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false  ];
     $pdo = new PDO($dsn, $user, $password, $options);
+	
+	//Lägg till require "config.php"; i de filer som ska komma åt databasen.
 
 ?>
