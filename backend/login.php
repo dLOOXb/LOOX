1e2c5a4ea492930956a797227a8f45f6e0272dbe
kattogat <maria_hav@live.se>
2017-02-07 12:55:33 +0100

    Buggar lösta

--- backend/login.php
+++ backend/login.php
@@ -5,8 +5,6 @@
 	session_start(); //Session så att inloggnigen sparas mellan de olika sidorna
 	require "config.php";
 	
-
-	
 	//Om någon trycker på knappen
 	if (isset($_POST['submitLogin'])) {
 		
@@ -23,20 +21,21 @@
 		
 			
 			//Stämmer användarnamnet överens med db
-			$sql = "SELECT id, count(anvandarnamn) AS antalrader FROM inlogg WHERE anvandarnamn = :useruo"; // AND lossenord = :seruo
+			$sql = "SELECT id, lossenord, count(anvandarnamn) AS antalrader FROM inlogg WHERE anvandarnamn = :useruo"; // AND lossenord = :seruo
 			$statement = $pdo->prepare($sql);
 			$statement->execute (array(':useruo' => $user)); //, ':seruo' => $pass
 			
 			$result = $statement->fetch(PDO::FETCH_ASSOC);
-			echo $result['antalrader'];
+			$hass = $result['lossenord'];
 			
 			
 			//Om värdet från databasen stämmer överäns med värdet från input  
-			if ($result['antalrader'] == 1 && password_verify($pass, $hass) === true) {
+			if (($result['antalrader'] == 1) && (password_verify($pass, $hass) == true)) {
 				$_SESSION['id'] = $result['id'];
 				$_SESSION['anvandarnamn'] = $user;
 			
-			echo "<p>Du är inloggad!</p>";
+				echo "<p>Du är inloggad!</p>";
+				
 			} else {
 				//Om värdet från databasen inte stämmer med värdet från input
 				die("Anvädarnamnet eller lösenordet stämmer inte överäns.");
