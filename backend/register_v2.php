8f8e37931c05f392591c0226c19c70d778b75d46
kattogat <maria_hav@live.se>
2017-02-09 11:48:47 +0100

    update

--- backend/register_v2.php
+++ backend/register_v2.php
@@ -2,6 +2,11 @@
 	session_start();
 	require "config.php";
 	
+	/*$data = json_decode(file_get_contents("php://input"));
+		print_r($jjson);
+		echo json_last_error();
+		echo $data; */
+	
 	//Har någon tryckt på kanppen
 	if (isset($_POST['submitReg'])){
 		
@@ -53,8 +58,8 @@
 		$hashpass = password_hash($pass, PASSWORD_DEFAULT);
 		
 		//släng in inlogginfo i db
-		$sql3 = "INSERT INTO inlogg (anvandarnamn, fornamn, efternamn, email, lossenord, tel, klass)
-            VALUES(:useruo, :foruo, :efteruo, :mailuo, :lossuo, :teluo, kund)";
+		$sql3 = "INSERT INTO inlogg (anvandarnamn, fornamn, efternamn, email, lossenord, tel)
+            VALUES(:useruo, :foruo, :efteruo, :mailuo, :lossuo, :teluo)";
 		$inlogg_intoDb = $pdo->prepare($sql3);
 		$inlogg_intoDb->execute (array(':useruo' => $user, ':foruo' => $fornamn, ':efteruo' => $efternamn, ':mailuo' => $epost, ':lossuo' => $hashpass, ':teluo' => $tel)); 
 		
@@ -78,3 +83,23 @@
 		
 	}
 ?>
+
+<html>
+<head></head>
+<body>
+	<form action="register_v2.php" method="post">
+	<label>Namn</label>
+	<input type="text" name="username">
+	<label>Lössenord</label>
+	<input type="password" name="password">
+	<label>E-mail</label>
+	<input type="text" name="email">
+	<label>Telefonnummer</label>
+	<input type="text" name="tel">
+	<label>Förnamn</label>
+	<input type="text" name="fornamn">
+	<label>Efternamn</label>
+	<input type="text" name="efternamn">
+	<input type="submit" name="submitReg" value="Logga in">
+</body>
+</html>
