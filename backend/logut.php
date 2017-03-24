3b31e7a76d9e55428c9ee1ee307c5a9aa8e38c45
kattogat <maria_hav@live.se>
2017-03-24 18:06:35 +0100

    Fixad minus stavfel på filnamn

--- backend/logut.php
+++ backend/logut.php
@@ -4,24 +4,10 @@
 	//Sesson kopplad till användarnas id för tabell.
 	session_start();
 	require "config.php";
-	
-	header('Access-Control-Allow-Origin: *');
 
 	//Om knapptryck...
 	if (isset($_POST['loggout'])) {
 		echo "Du är utloggad";
 		//Avslutar sessionen och tar bort id värdet från $_SESSION
-		session_unset(); 
 		session_destroy();
 	}
-
-?>
-
-<html>
-<head>
-</head>
-<body>
-	<form action="logut.php" method="post">
-	<input type="submit" name="loggout" value="Logga ut">
-</body>
-</html>
