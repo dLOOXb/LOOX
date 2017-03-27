3b7d7a09c10430bde62300c29ffcf7c40dff999b
kattogat <maria_hav@live.se>
2017-03-27 19:41:22 +0200

    Fixad

--- backend/saloger.php
+++ backend/saloger.php
@@ -1,34 +1,22 @@
 <?php 
-	session_start();
 	require "config.php";
 
 	//Loopar igenom alla salongnamn som finns i DB och skriver ut dem som json.
-	header('Access-Control-Allow-Origin: *');
 
     $sql = "SELECT salongnamn, url, info, tel, gata, postnummer, ort FROM salong"; 
     $statement = $pdo->query($sql);
     foreach( $statement as $row ) {
-		$salong = $row['salongnamn']; 
-        $url = $row['url']; 
-		$info = $row['info'];
-		$tel = $row['tel'];
-		$gata = $row['gata'];
-		$postnum = $row['postnummer'];
-		$ort = $row['ort'];
-	
-	
 		$data[] = array(
-			'salong' => $salong,
-            'url' => $url,
-			'info' => $info,
-			'tel' => $tel,
-			'gata' => $gata,
-			'postnummer' => $postnum,
-			'ort' => $ort,
+			'salong' => $row['salongnamn'],
+            'url' => $row['url'],
+			'info' => $row['info'],
+			'tel' => $row['tel'],
+			'gata' => $row['gata'],
+			'postnummer' => $row['postnummer'],
+			'ort' => $row['ort'],
 		);
 		
     } 
 	
+	header("Content-Type:application/json:charset=utf-8");
 	echo json_encode($data);
-
-?>
