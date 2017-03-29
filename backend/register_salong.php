ed4461bc666f505df1358487fd66ab0852f247d6
Maria Berntsson <kattogat@users.noreply.github.com>
2017-03-29 08:39:14 +0200

    Bortglömd kod fixad

--- backend/register_salong.php
+++ backend/register_salong.php
@@ -58,6 +58,7 @@
 		} 
 		
 		//Inga fel?!? Let's goooooooooooo!!!!!!
+		/* if($formIsOk === true) { */
 		
 		//Skapar hashed lössenord med default inställnigar.
 		$hashpass = password_hash($pass, PASSWORD_DEFAULT);
@@ -79,6 +80,8 @@
 		$loggin = $pdo->prepare($sql5);
 		$loggin->execute (array(':useruo' => $user)); 
 		$id = $loggin->fetch(PDO::FETCH_ASSOC);
+		
+		//}
 		
 		//Om inloggnig misslyckas
 		if ($loggin == NULL) {
